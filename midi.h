9a4e4aad1a9360412db47877574ca2156017ed4c
aarnav <aarnavbos@gmail.com>
2022-11-14 14:31:10 +0100

    init commit

---
+++ midi.h
@@ -0,0 +1,6 @@
+#ifndef Midi_h
+#define Midi_h
+uint8_t CLOCK_MSG = 248;
+uint8_t STOP_MSG = 252;
+uint8_t PULSES_IN_BAR = 48;
+#endif
---
+++ state.h
@@ -0,0 +1,9 @@
+#ifndef AppState_h
+#define AppState_h
+struct APPState
+{
+  long bars;
+  uint8_t pulses; // resets every bar
+  bool optionMenu;
+};
+#endif
